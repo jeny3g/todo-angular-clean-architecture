4ca6972ae051b5a3c2e975c2d8cd5b4880947387
jeny3g <mirandajean2009@gmail.com>
2024-02-05 22:06:55 -0300

    feat: :sparkles: removendo pacotes

--- src/app/presentation/view/pages/home/home.component.ts
+++ src/app/presentation/view/pages/home/home.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ITaskController } from '../../../../domain/interfaces/controllers/itask-controller';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   title = 'Lista';
-  constructor(private titleService: Title) {
+  constructor(
+    private titleService: Title,
+    private service: ITaskController
+  ) {
     this.titleService.setTitle($localize`${this.title}`);
+  }
+  ngOnInit(): void {
+    // console.log(this.service.get());
   }
 }
